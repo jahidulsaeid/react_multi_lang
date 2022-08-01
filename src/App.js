d560a734d8950ba6a2c4be85ede812110f1206e8
jahidulsaeid <jahidul.saeid@gmail.com>
2022-08-01 09:26:51 +0600

    add i18n

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useTranslation } from 'react-i18next';
+
 
 function App() {
+  const { t, i18n } = useTranslation();
+
+  const langChange = lng => {
+    i18n.changeLanguage(lng);
+  }
+
   return (
     <div className="App">
+      <div>
+        <button onClick={() => langChange('bn')}>Bangla</button>
+        <button onClick={() => langChange('en')}>English</button>
+        <button onClick={() => langChange('de')}>Dutch</button>
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>{t('Hello')}</h1>
+        <p>{t('I am')}</p>
+        <p>{t('Greeting')}</p>
+        <p></p>
         <a
           className="App-link"
           href="https://reactjs.org"
